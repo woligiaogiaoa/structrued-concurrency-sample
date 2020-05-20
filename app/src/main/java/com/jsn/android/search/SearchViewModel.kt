83598a3ec9fb94d0e073335fbbab084d9ba7fdda
admin <450121066@qq.com>
2020-05-20 20:57:50 +0800

    补充注释

--- app/src/main/java/com/jsn/android/search/SearchViewModel.kt
+++ app/src/main/java/com/jsn/android/search/SearchViewModel.kt
@@ -10,9 +10,12 @@ class SearchViewModel(val repository: SearchSource):ViewModel(){
 
     val keyWord=MutableLiveData<String>()
 
+    //默认是主线程，如果代码存在阻塞风险，可以随时切换线程
+    //使用 liveData这个builder。不用管是在哪个线程 直接emit。
+
     val searchResult: LiveData<Result<List<String>>> = keyWord.switchMap { keyWord ->
-        liveData(Dispatchers.Default) {        //默认是主线程，如果代码存在阻塞风险，可以随时切换线程
-            emit(Result.Loading)               //使用 liveData这个builder。不用管是在哪个线程 直接emit。
+        liveData(Dispatchers.Default) {
+            emit(Result.Loading)
             emit(try {
                 Result.Success(repository.search(keyWord))
             }catch (e:Exception){
